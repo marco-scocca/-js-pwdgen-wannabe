58122dcc2a0fb1d23424e72762dfcb76b10e3a7c
marco-scocca <marcoscocca1991@gmail.com>
2024-06-25 09:50:27 +0200

    number23

--- script/script.js
+++ script/script.js
@@ -2,10 +2,10 @@ let first_name = prompt('inserisci il tuo nome');
 let second_name = prompt('inserisci il tuo cognome');
 let colour = prompt('inserisci il tuo colore preferito');
 
-document.getElementById('template_literal').innerText = `${first_name} ${second_name} ${colour}`;
+document.getElementById('template_literal').innerText = `${first_name} ${second_name} ${colour}23`;
 
 const STRING = first_name;
 console.log(STRING);
-let full_string = `${first_name} ${second_name} ${colour}`;
+let full_string = `${first_name} ${second_name} ${colour}23`;
 console.log(full_string);
 
