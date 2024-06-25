9677f5d3449ec4aec4c77c1df5fd8871605c7968
marco-scocca <marcoscocca1991@gmail.com>
2024-06-25 09:11:53 +0200

    colour

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
-document.writeln('messaggio con writeln');
-
-
 let first_name = prompt('inserisci il tuo nome');
 let second_name = prompt('inserisci il tuo cognome');
+let colour = prompt('inserisci il tuo colore preferito');
 
-document.getElementById('template_literal').innerText = `${first_name} ${second_name}`;
+document.getElementById('template_literal').innerText = `${first_name} ${second_name} ${colour}`;
 
 const STRING = first_name;
 console.log(STRING);
-let full_string = `${first_name} ${second_name}`;
+let full_string = `${first_name} ${second_name} ${colour}`;
 console.log(full_string);
+
