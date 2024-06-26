dc548929a7a917210f79bfba8cebfb8daf9dc2f1
marco-scocca <marcoscocca1991@gmail.com>
2024-06-26 07:54:33 +0200

    fix-exercise

--- script/script.js
+++ script/script.js
@@ -1,11 +1,7 @@
 let first_name = prompt('inserisci il tuo nome');
 let second_name = prompt('inserisci il tuo cognome');
 let colour = prompt('inserisci il tuo colore preferito');
-
 document.getElementById('template_literal').innerText = `${first_name} ${second_name} ${colour}23`;
-
-const STRING = first_name;
-console.log(STRING);
 let full_string = `${first_name} ${second_name} ${colour}23`;
-console.log(full_string);
+
 
